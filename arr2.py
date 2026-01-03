b6aba02a807192fd956a8b55851734ab49c8196a
sakshiinaik18-svg <sakshiinaik18@gmail.com>
2026-01-03 12:14:07 +0530

    array code

--- arr2.py
+++ arr2.py
@@ -1,1 +1,11 @@
+def main():
+    scores=[78,99,86,66,52]
+    total=sum(scores)
+    average=total/len(scores)
+    print("== main/master branch output ==")
+    print(f"score: {scores}")
+    print(f"total: {total}")
+    print(f"average: {average}")
 
+if __name__ == "__main__":
+    main()
