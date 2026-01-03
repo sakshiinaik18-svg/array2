2955a8be7f38d1fd722d278791c07f14cb283867
sakshiinaik18-svg <sakshiinaik18@gmail.com>
2026-01-03 12:26:20 +0530

    modified

--- arr2.py
+++ arr2.py
@@ -6,6 +6,8 @@ def main():
     print(f"score: {scores}")
     print(f"total: {total}")
     print(f"average: {average}")
-
+    print("== local branch output ==")
+    print(f"Maximum={max(scores)}")
+    print(f"Minimum={min(scores)}")
 if __name__ == "__main__":
     main()
